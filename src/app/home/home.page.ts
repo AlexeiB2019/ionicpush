e56e51ac9ea0348051f8be10c2f78232f2280f02
Alexei Bushuev <alexeib.2019@gmail.com>
2021-01-26 14:59:47 +0000

    added button to test post requqest

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private httpOptions = {}
 
-  constructor() {}
+  constructor( private http: HttpClient ) {
+  	this.httpOptions = {
+  	  headers: new HttpHeaders( { 'Content-Type': 'text/html',
+  	                              'Access-Control-Allow-Origin': '*',
+  	                              'Cache-Control': 'no-cache' } )
+  	}
+  }
 
+  testBtn() {
+  	let url = 'https://push.freshgrillfoods.com/serviceworker/register'
+
+  	// let request = { token: '12345' }
+  	let request = "token=12345"
+  	this.http.post(url, request, this.httpOptions)
+  	         .subscribe( response => {
+  	         	alert(response)
+  	         })
+  }
 }
